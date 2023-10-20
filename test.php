71d47f916ad86031e7a74ff5a5b75b2d2b149528
vananhpts <21211tt2369@mail.tdc.edu.vn>
2023-10-20 11:13:34 +0700

    Create test.php

---
+++ test.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis minus quae culpa praesentium ipsum aperiam, aliquam laborum voluptatum voluptates. Soluta sunt vel eaque quo cupiditate laboriosam eum asperiores accusamus nemo!</P>
+</body>
+</html>
